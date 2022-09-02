6781b0312bcd2822760e3d0f4da1d64f2aed252f
Hnzh612 <15120376892@163.com>
2022-09-02 17:36:56 +0800

    1

---
+++ 前端/financialmanagement/src/components/utlis/notype.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <img src="@/assets/notype.png" class="notype">
+    <p class="words">抱歉！您无权访问该页面，请联系管理员</p>
+</div>
+</template>
+
+
+<script>
+export default{
+    name:"notype"
+}
+</script>
+
+
+<style lang="less" scoped>
+    .notype {
+        width: 300px;
+        margin:4% calc(50% - 150px)
+    }
+    .words {
+        text-align: center; 
+        margin-top: 50px; 
+        font-size: 18px;
+    }
+</style>
